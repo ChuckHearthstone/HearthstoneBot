fce299528633aa8f3c9da589904f86462c9d4d5e
Chuck Lu <chuck.lu@qq.com>
2019-09-15 13:21:40 +0800

    add ns3.Class9.smethod_1

---
+++ HearthstoneBot/Class9.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading;
+
+namespace HearthstoneBot
+{
+    public class Class9
+    {
+        private static Mutex smethod_1(int int_0, out bool bool_0)
+        {
+            var mutexName = "Local\\" + (Environment.MachineName.GetHashCode() ^ int_0.GetHashCode() ^
+                                         TimeZone.CurrentTimeZone.StandardName.GetHashCode() ^
+                                         "Blizz_HS_BuddyTeam".GetHashCode() + 25);
+            var mutex = new Mutex(true, mutexName, out bool_0);
+            return mutex;
+        }
+
+    }
+}

c92acb214c8adb8185464a4eb5ec5fdb2bceeb5a
Chuck Lu <chuck.lu@qq.com>
2019-09-15 13:26:05 +0800

    add method smethod_6

--- HearthstoneBot/Class9.cs
+++ HearthstoneBot/Class9.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 
 namespace HearthstoneBot
@@ -14,5 +17,26 @@ namespace HearthstoneBot
             return mutex;
         }
 
+        private static bool smethod_6(Process process_0)
+        {
+            try
+            {
+                var certificate = X509Certificate.CreateFromSignedFile(process_0.MainModule.FileName);
+                var flag1 = certificate.Subject.Contains("Blizzard Entertainment, Inc.");
+
+                var processPath = Path.GetDirectoryName(process_0.MainModule.FileName);
+                var dataPath = Path.Combine(processPath, "Hearthstone_Data");
+                var flag2 = Directory.Exists(dataPath);
+
+                if (flag1 && flag2)
+                {
+                    return true;
+                }
+            }
+            catch
+            {
+            }
+            return false;
+        }
     }
 }

4f3dea59f5682305de95ca8050fe85aa6fce8bf7
Chuck Lu <chuck.lu@qq.com>
2019-09-15 23:27:11 +0800

    add default bot

---
+++ HearthstoneBot/Bot/DefaultBot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace HearthstoneBot.Bot
+{
+    class DefaultBot : IBot
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Tick()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Deinitialize()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

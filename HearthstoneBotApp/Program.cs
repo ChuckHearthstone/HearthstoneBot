337cfdffb645072f32351d488a347ed06e177776
Chuck Lu <chuck.lu@qq.com>
2019-10-19 13:02:01 +0800

    handle unhanlde exception

--- HearthstoneBotApp/Program.cs
+++ HearthstoneBotApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace HearthstoneBotApp
@@ -11,9 +12,25 @@ namespace HearthstoneBotApp
         [STAThread]
         static void Main()
         {
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += Application_ThreadException;
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new FormMain());
+        }
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            DealException(e.Exception);
+        }
+
+        static void DealException(Exception ex)
+        {
+            if (ex != null)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
